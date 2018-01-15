25ab853cd791347ea306aa5f363d83fa78c3bd36
GuillermoAdrianU <35458662+GuillermoAdrianU@users.noreply.github.com>
2018-01-15 09:46:55 -0600

    Código completo de NuevoHelado

---
+++ Nuevo helado.py
@@ -0,0 +1,18 @@
+#Autor: Guillermo Adrian Urbina Aguiñiga
+#Calcula el área de im helado (en 2D)
+
+radio = int(input("Teclea el radio: "))
+altura = int(input("Teclea la altura: "))
+
+area = (3.141592*radio**2) / 2 + radio*altura
+
+print("Area =", area)
+
+
+
+
+
+
+
+
+
